93c38a05f188842fb125b9155e9b66b04becbb16
labgeek010 <119995197+labgeek010@users.noreply.github.com>
2024-03-23 20:59:24 -0600

    js fix, nav bar working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ window.addEventListener("scroll", () => {
 });
 
 const headerMenu = document.querySelector(".header__menu"),
- menuBtn = document.querySelector(".menu .btn"),
- headerMenuItems = headerMenu.querySelectorAll("li a");
+    menuBtn = document.querySelector(".menu-btn"),
+    headerMenuItems = headerMenu.querySelectorAll("li a");
 
     menuBtn.addEventListener("click", () => {
     headerMenu.classList.toggle("show");
